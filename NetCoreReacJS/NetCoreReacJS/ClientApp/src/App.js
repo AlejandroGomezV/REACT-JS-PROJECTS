1315e977697e1751dd258c4ad0e080cb411a944c
Alejandro Gomez <m.alejandro.gomez.v@gmail.com>
2022-12-08 21:40:58 -0600

    UseState
    Is util to update components without refresh the page

--- NetCoreReacJS/NetCoreReacJS/ClientApp/src/App.js
+++ NetCoreReacJS/NetCoreReacJS/ClientApp/src/App.js
@@ -1,9 +1,38 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 import Card from "./Components/Card";
 
 const App = () => {
+
+    const [number, setNumber] = useState(0);
+
+    /*
+     number = variable for get value | actual state
+     setNumber = function for update value | update state
+     */
+
+    let modelPerson = {
+        Name: "Maria",
+        Email: "maria@gmail.com"
+    }
+
+    const [person, setPerson] = useState(modelPerson);
+
     return (
         <div className="container-fluid">
+            <h1>Actual value: {number}</h1>
+            <button onClick={() => setNumber(number + 1)}>+1</button>
+
+            <br />
+            <br />
+            <p>Name: {person.Name}</p>
+            <p>Email: {person.Email}</p>
+            <button onClick={() => setPerson({
+                ...person,
+                Email:"maria2@gmail.com"
+            })}>+1</button>
+
+
             <div className="row justify-content-sm-center">
                 <div className="col-sm-4">
                     <Card title="REACT PROJECT WITH NET CORE" paragraph="First exercise whit REACT JS and NET CORE" btntext="Go to Github">
